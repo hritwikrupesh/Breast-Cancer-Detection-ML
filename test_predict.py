3a7f9ec2c1ea9bfbefdf73b0f4cee4de70ff25a3
hritwikrupesh <hritwikrupeshgollu@gmail.com>
2026-07-05 10:35:45 +0530

    Remove unused module

--- test_predict.py
+++
@@ -1,8 +0,0 @@
-from src.predict import predict_label
-import pandas as pd
-
-df = pd.read_csv("data/processed/breast_cancer_processed.csv")
-
-sample = df.drop("target", axis=1).iloc[[0]]
-
-print("Prediction:", predict_label(sample))
